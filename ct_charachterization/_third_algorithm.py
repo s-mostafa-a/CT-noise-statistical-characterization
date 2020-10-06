e74024e2210f8594c4f2da247686c29abd96e7c9
Mostafa <s.mostafa.a96@gmail.com>
2020-10-06 16:41:56 +0330

    debug based on paper

--- ct_charachterization/_third_algorithm.py
+++ ct_charachterization/_third_algorithm.py
@@ -11,6 +11,15 @@ def run_third_algorithm(y: np.array, mu: np.array, neighborhood_size: int, delta
         y = y - delta
     theta, gamma = run_second_algorithm(y, mu=mu, neighborhood_size=neighborhood_size, delta=delta, max_iter=max_iter,
                                         tol=tol)
+    pi = theta[0, ...]
+    axes = list(range(len(pi.shape)))
+    axes.pop(0)
+    axes.append(0)
+    pi = np.transpose(pi, axes=axes)
+    # print('pi:', pi.shape)
+    times_pi_to_be_broad_casted = np.array(np.array(y.shape) / np.array(pi.shape[:-1]), dtype=int)
+    pi = broadcast_tile(pi, tuple(list(times_pi_to_be_broad_casted) + [1]))
+    # print('pi:', pi.shape)
     shape_of_each_neighborhood = tuple([neighborhood_size for _ in y.shape])
     blocked_y = block_matrix(mat=y, neighborhood_shape=shape_of_each_neighborhood)
     blocked_radical_y = block_matrix(mat=np.sqrt(y), neighborhood_shape=shape_of_each_neighborhood)
@@ -28,8 +37,9 @@ def run_third_algorithm(y: np.array, mu: np.array, neighborhood_size: int, delta
                                                shape_of_each_neighborhood)
         first_local_sample_conditioned_moment[..., j] = first_numerator_summation / denominator_summation
         second_local_sample_conditioned_moment[..., j] = second_numerator_summation / denominator_summation
+    first_local_sample_conditioned_moment = np.sum(first_local_sample_conditioned_moment * pi, axis=-1)
+    second_local_sample_conditioned_moment = np.sum(second_local_sample_conditioned_moment * pi, axis=-1)
     local_sample_variance = second_local_sample_conditioned_moment - np.power(first_local_sample_conditioned_moment, 2)
-    y_stab = (constant_c * (np.expand_dims(np.sqrt(y),
-                                           axis=-1) - first_local_sample_conditioned_moment) / np.sqrt(
+    y_stab = (constant_c * (np.sqrt(y) - first_local_sample_conditioned_moment) / np.sqrt(
         local_sample_variance)) + second_local_sample_conditioned_moment
     return y_stab
--- run.py
+++ run.py
@@ -1,39 +1,35 @@
 import numpy as np
-from ct_charachterization import run_first_algorithm, _run_third_algorithm_without_fixed_neighborhood, \
-    run_second_algorithm
+from ct_charachterization import run_first_algorithm, run_second_algorithm, run_third_algorithm
 import matplotlib.pyplot as plt
 
 
 def test_first():
-    MU = np.array([-1000, -700, -90, 50, 300])
+    mu = np.array([-1000, -700, -90, 50, 300])
     # length of pixels vector
-    N_1 = 20
+    n_1 = 20
     np.random.seed(1)
-    X_1 = np.random.randint(low=-1000, high=400 + 1, size=N_1)
-    theta_1, gamma_1 = run_first_algorithm(y=X_1, mu=MU, non_central=True, neighborhood_size=10)
+    x_1 = np.random.randint(low=-1000, high=400 + 1, size=n_1)
+    theta_1, gamma_1 = run_first_algorithm(y=x_1, mu=mu, non_central=True, neighborhood_size=10)
     print(theta_1.shape)
 
 
 def test_second():
-    MU = np.array([-1000, -700, -90, 50, 300])
+    mu = np.array([-1000, -700, -90, 50, 300])
     img = np.load(f'''./resources/2d_img.npy''')
-    X_2 = img
-    theta_2, gamma_2 = run_second_algorithm(y=X_2, mu=MU, non_central=True, neighborhood_size=32)
+    x_2 = img
+    theta_2, gamma_2 = run_second_algorithm(y=x_2, mu=mu, non_central=True, neighborhood_size=32)
     print(theta_2.shape)
 
 
 def test_third():
-    mu_5 = np.array([-870])
+    mu = np.array([-987, -880, -540, -370, -160, 0, 100, 240, 340])
     img = np.load(f'''./resources/luna_cropped.npy''')
     print(img.shape)
     img = img[0:128, 0:128]
     plt.imshow(img, cmap='gray')
     plt.show()
-    plt.imshow(img[16:112, 16:112], cmap='gray')
-    plt.show()
-    stabilized_y = _run_third_algorithm_without_fixed_neighborhood(img, mu_5, non_central=True, constant_c=10,
-                                                                   neighborhood_size=32)
-    sy = stabilized_y[..., 0]
+    stabilized_y = run_third_algorithm(img, mu, non_central=True, constant_c=10, neighborhood_size=32)
+    sy = stabilized_y
     print(sy.shape)
     plt.imshow(sy, cmap='gray')
     plt.show()
